004eb11f5e200519140ad52815fb7c5f3c492a38
Luc.Nguyen <luc.nguyen@kiaisoft.com>
2024-08-06 11:48:36 +0700

    (feature) create chat model and view chat for user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\ChatMessage;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,7 +10,33 @@ Route::get('/', function () {
 
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard', [
+        'users' => User::query()->whereNot('id', auth()->id())->get()
+    ]);
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::get('/chat/{friend}', function (User $friend) {
+    return view('chat', [
+        'friend' => $friend
+    ]);
+})->middleware(['auth'])->name('chat');
+
+Route::get('messages/{friend}', function (User $friend) {
+    return ChatMessage::query()->where(function ($query) use ($friend) {
+        $query->where('sender_id', auth()->id())->where('receiver_id', $friend->id);
+    })->orWhere(function ($query) use ($friend) {
+        $query->where('sender_id', $friend->id)->where('receiver_id', auth()->id());
+    })->with(['sender', 'receiver'])
+        ->orderBy('id', 'asc')
+        ->get();
+})->middleware(['auth']);
+
+Route::post('messages/{friend}', function (User $friend) {
+    return ChatMessage::query()->create([
+        'sender_id' => auth()->id(),
+        'receiver_id' => $friend->id,
+        'text' => request('message')
+    ]);
+})->middleware(['auth']);
+
+require __DIR__ . '/auth.php';
